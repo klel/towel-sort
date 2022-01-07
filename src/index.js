2df6dbfcc3eef15fdd82df2c8abbc2dafff3083f
devEl <klimelm@gmail.com>
2022-01-07 11:19:38 +0300

    init: task solved (with no recursion)

--- src/index.js
+++ src/index.js
@@ -2,5 +2,19 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    if(!matrix)
+        return [];
+        
+    let r = matrix.reduce((acc, curr, i) => {
+        if(Array.isArray(curr)){
+            curr = i % 2 !== 0 ? curr.reverse() : curr;
+            acc.push(...curr);
+        }
+        else
+            acc.push(curr);
+        return acc;
+    }, []);
+
+    
+    return r;
 }
